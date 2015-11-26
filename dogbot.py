bacf2cc33c5ce814be0c92a328fa374b5ada5a7d
Jacob Greenway <jagreenway12@gmail.com>
2015-11-26 14:35:02 -0600

    added sounds, other import files, and main loop structure

--- dogbot.py
+++ dogbot.py
@@ -1,8 +1,28 @@
 import motors
+import switch
+import ultrasound
+import sound
 
-motors.reset()
-motors.forward(2)
-motors.backup(1)
+bool freeMode = false
 
+# Main loop for polling dogbot sensors
+while (True):
+
+    # Checks to see if the button to toggle freeMode is on
+    if (switch.GPIO.input() == 1):
+        freeMode = !freeMode
+    
+    # Runs dogbot based on freeMode setting
+    if (freeMode):
+        if (ultrasound.object()):
+            motors.backup(2)
+            motors.turnRight(1)
+        else:
+            motors.forward(2)
+    else:
+        if (ultrasound.object()):
+            motors.turnLeft(3)
+            sound.bark()
+        
 motors.GPIO.cleanup()
 exit()
